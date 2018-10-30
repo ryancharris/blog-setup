26d2fe0749d991bdf46eb7ba60d4503c72bdbae1
Ivana Veliskova <iveliskova@getguru.com>
2018-10-29 22:52:42 -0400

    Fixes eslint and updates styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,29 @@ import { Link } from 'gatsby'
 import './Header.css'
 
 class Header extends Component {
+  state = {
+    menuOpen: false,
+  }
+
+  handleClick = () => {
+    this.setState(state => {
+      const menuOpen = state.menuOpen === false ? true : false
+
+      return { menuOpen }
+    })
+  }
+
   renderMenu = () => {
-    return <div className="Header__menu" />
+    return <div className="Header__menu">Menu</div>
   }
 
   render() {
     const { className } = this.props
+    const { menuOpen } = this.state
 
-    const cn = 'Header ' + className
+    const blur = menuOpen ? 'is-blurred' : null
+
+    const cn = 'Header ' + className + ' ' + blur
 
     return (
       <div className={cn}>
@@ -21,11 +36,12 @@ class Header extends Component {
         <div
           className="Header__nav"
           onClick={() => {
-            console.log('clicked')
+            this.handleClick()
           }}
         >
           <div className="Header__nav-hamburger" />
         </div>
+        {menuOpen && this.renderMenu()}
       </div>
     )
   }
