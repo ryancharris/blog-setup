0d26877a9512a0368fbc7f2eaf85be4c75deef9f
Ryan Harris <harrisryan1@gmail.com>
2018-10-30 16:20:00 -0400

    Remove Bio component

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,7 +3,6 @@ import Helmet from 'react-helmet'
 import { Link, graphql } from 'gatsby'
 import get from 'lodash/get'
 
-import Bio from '../components/Bio'
 import Layout from '../components/layout'
 import HeroBlock from '../components/HeroBlock'
 
@@ -27,25 +26,12 @@ class BlogPostTemplate extends React.Component {
           title={post.frontmatter.title}
           date={post.frontmatter.date}
         />
-        {/* <h1>{post.frontmatter.title}</h1>
-        <p
-          style={{
-            ...scale(-1 / 5),
-            display: 'block',
-            marginBottom: rhythm(1),
-            marginTop: rhythm(-1),
-          }}
-        >
-          {post.frontmatter.date}
-        </p> */}
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
         <hr
           style={{
             marginBottom: rhythm(1),
           }}
         />
-        <Bio />
-
         <ul
           style={{
             display: 'flex',
