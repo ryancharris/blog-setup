2e2612df5a71c3852d74e6a5254d244a59fc1d9e
Ryan Harris <harrisryan1@gmail.com>
2018-10-30 23:36:35 -0400

    Abstract helmet metadata to layout component

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link, graphql } from "gatsby";
 import get from "lodash/get";
-import Helmet from "react-helmet";
 
 import Layout from "../components/layout";
 import HeroBlock from "../components/HeroBlock";
@@ -11,20 +10,10 @@ import { rhythm } from "../utils/typography";
 
 class BlogIndex extends React.Component {
   render() {
-    const siteTitle = get(this, "props.data.site.siteMetadata.title");
-    const siteDescription = get(
-      this,
-      "props.data.site.siteMetadata.description"
-    );
     const posts = get(this, "props.data.allMarkdownRemark.edges");
 
     return (
       <Layout location={this.props.location}>
-        <Helmet
-          htmlAttributes={{ lang: "en" }}
-          meta={[{ name: "description", content: siteDescription }]}
-          title={siteTitle}
-        />
         <HeroBlock title="My Blog!" />
         <PageBody>
           {posts.map(({ node }) => {
@@ -55,12 +44,6 @@ export default BlogIndex;
 
 export const pageQuery = graphql`
   query {
-    site {
-      siteMetadata {
-        title
-        description
-      }
-    }
     allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       edges {
         node {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,4 @@
 import React from "react";
-import { Link } from "gatsby";
-import get from "lodash/get";
-import Helmet from "react-helmet";
 
 import Layout from "../components/layout";
 
@@ -9,19 +6,8 @@ import "./Main.css";
 
 class Home extends React.Component {
   render() {
-    const siteTitle = get(this, "props.data.site.siteMetadata.title");
-    const siteDescription = get(
-      this,
-      "props.data.site.siteMetadata.description"
-    );
-
     return (
       <Layout location={this.props.location}>
-        <Helmet
-          htmlAttributes={{ lang: "en" }}
-          meta={[{ name: "description", content: siteDescription }]}
-          title={siteTitle}
-        />
         <div className="Main">
           <h1 className="Main__header">It me_</h1>
         </div>
